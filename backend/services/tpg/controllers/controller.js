30ab28585b939cbe46b7812192246694d8a13bc2
jansmailbegovic <jan.smailbegovic@gmail.com>
2024-04-06 13:02:37 -0400

    create training plan framework

--- backend/services/tpg/controllers/controller.js
+++ backend/services/tpg/controllers/controller.js
@@ -2,17 +2,17 @@ const generator = require('../logic/generator');
 
 // controller function to handle the request for generating a training plan
 function generateTrainingPlan(req, res) {
-  try {
-    const userInput = req.body;
-    const trainingPlan = generator.generateTrainingPlan(userInput);
-    res.status(200).json(trainingPlan);
-    
-  } catch (error) {
-    console.error('Error generating training plan:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
+    try {
+        const userInput = req.body;
+        const trainingPlan = generator.generateTrainingPlan(userInput);
+        res.status(200).json(trainingPlan);
+
+    } catch (error) {
+        console.error('Error generating training plan:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 }
 
 module.exports = {
-  generateTrainingPlan,
+    generateTrainingPlan,
 };
--- backend/services/tpg/logic/generator.js
+++ backend/services/tpg/logic/generator.js
@@ -1,17 +1,67 @@
 // generate a training plan based on user input
 function generateTrainingPlan(userInput) {
-    
-    // TODO: Implement the logic to generate a training plan based on user input
-  
-    console.log("generating plan...")
-    return {
-        "race": "marathon",
-        "duration": 16,
-        "weekly-activities": 4
+    console.log(userInput);
+
+    const weeks = parseInt(userInput.weeks);
+    const frequency = parseInt(userInput.frequency);
+    const target = parseInt(userInput.target);
+    const firstLongRun = userInput.firstLongRun;
+    const longRunDay = userInput.longRunDay;
+
+    const workoutDays = getWorkoutDays(frequency, longRunDay);
+    const workoutTypes = getWorkoutTypes(frequency);
+
+    for (let i = 0; i < weeks; i++) {
+        console.log(`Week ${i+1}: `);
+        for (let j = 0; j < frequency; j++) {
+            console.log(` > ${workoutDays[j]}: ${workoutTypes[j]}`);
+        }
     }
-  }
+
+    return { "test": "pass" };
+}
+
+function getWorkoutDays(frequency, longRunDay) {
+
+    if (longRunDay == 'saturday') {
+        switch (frequency) {
+            case 2: return ['wednesday', 'saturday'];
+            case 3: return ['tuesday', 'thursday', 'saturday'];
+            case 4: return ['monday', 'tuesday', 'thursday', 'saturday'];
+            case 5: return ['monday', 'tuesday', 'wednesday', 'friday', 'saturday'];
+            case 6: return ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+            case 7: return ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+            default: return [];
+        }
+    } else {
+        switch (frequency) {
+            case 2: return ['thursday', 'sunday'];
+            case 3: return ['tuesday', 'thursday', 'sunday'];
+            case 4: return ['tuesday', 'wednesday', 'friday', 'sunday'];
+            case 5: return ['tuesday', 'wednesday', 'thursday', 'saturday', 'saturday'];
+            case 6: return ['tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+            case 7: return ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+            default: return [];
+        }
+    }
+
+}
+
+function getWorkoutTypes(frequency) {
+
+    switch (frequency) {
+        case 2: return ['easy', 'long'];
+        case 3: return ['easy', 'tempo', 'long'];
+        case 4: return ['easy', 'tempo', 'easy', 'long'];
+        case 5: return ['easy', 'easy', 'tempo', 'easy', 'long'];
+        case 6: return ['easy', 'easy', 'tempo', 'easy', 'easy', 'long'];
+        case 7: return ['easy', 'easy', 'easy', 'tempo', 'easy', 'easy', 'long'];
+        default: return [];
+    }
+
+}
   
-  module.exports = {
+module.exports = {
     generateTrainingPlan,
-  };
+};
   
