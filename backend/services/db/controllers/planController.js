25a2c2e72f76ea1eb62faada5ab84f632ca5ef02
jansm04 <jan.smailbegovic@gmail.com>
2024-04-18 10:52:37 -0400

    implement create user

--- backend/services/db/controllers/planController.js
+++ backend/services/db/controllers/planController.js
@@ -1,12 +1,6 @@
 // insert training plan in db
 function createTrainingPlan(req, res) {
     // todo
-    const userInput = req.body;
-    const planID = userInput.planID;
-    res.status(200).json({
-        result: "success",
-        planID: planID
-    });
 }
 
 // get training plan with id in req
--- backend/services/db/controllers/userController.js
+++ backend/services/db/controllers/userController.js
@@ -1,6 +1,37 @@
+const connection = require('../config');
+
 // insert user into db
 function createUser(req, res) {
-    // todo
+    
+    const userInput = req.body;
+
+    const userID = userInput.userID;
+    const username = userInput.username;
+    const password = userInput.password;
+
+    // optional variables
+    const firstName = userInput.firstName ? userInput.firstName : null;
+    const lastName = userInput.lastName ? userInput.lastName : null;
+
+    try {
+        if (!userID || !username || !password)
+            return res.status(500).json({ error: "Missing required variables" });
+
+        const query = 'INSERT INTO stride.users ( user_id, username, password, first_name, last_name) VALUES (?, ?, ?, ?, ?);'
+        const values = [userID, username, password, firstName, lastName];
+
+        connection.query(query, values, (error, results) => {
+            if (error) {
+                console.log(error.sqlMessage);
+                return res.status(500).json({ error: error.sqlMessage });
+            }
+            console.log("User add succesfully.", results);
+            res.status(200).json({ result: "User add succesfully." });
+        });
+        
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
 }
 
 // get user with id in req
