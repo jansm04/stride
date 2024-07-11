9d9d76ef1a2327eec061db3bdb43cd2c359b0d3d
jansm04 <jan.smailbegovic@gmail.com>
2024-07-11 20:34:23 +0000

    fix plan router

--- backend/services/auth/routes/router.js
+++ backend/services/auth/routes/router.js
@@ -8,9 +8,6 @@ router.post('/register', controller.register);
 // route to a login a user
 router.post('/login', controller.login);
 
-// route to verify a user
-router.post('/verify', controller.verify);
-
 // route to refresh a token
 router.post('/refresh-token', controller.refreshToken);
 
