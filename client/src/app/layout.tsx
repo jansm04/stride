c1888bee190d90fd3006f8cebad535f199727540
jansm04 <jan.smailbegovic@gmail.com>
2024-08-23 16:37:20 -0400

    inits

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TrainingPlanProvider } from "@/lib/context/plan-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <TrainingPlanProvider>
+        <body className={inter.className}>{children}</body>
+      </TrainingPlanProvider>
     </html>
   );
 }
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello World</p>
+    <main className="bg-wavy-gradient flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Stride</h1>
+      <p>Get started by generating your custom training plan.</p>
+      <Link href="/generate">Generate Training Plan</Link>
     </main>
   );
 }
